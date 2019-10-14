98de226903698b960f8ad8981e4a05a89ce69679
chukwunonso chukwudozie <anonsoonye2001@yahoo.co.uk>
2019-10-13 21:04:15 -0500

    me

--- Lab-Summarize-and-Merge.R
+++ Lab-Summarize-and-Merge.R
@@ -160,10 +160,13 @@ c
 
 c[1,1]
 c[1,3]
-f
+
+
+#group by depth_fac
 f%>%group_by(area_fac,depth_fac)%>%summarise(parcel.lengthmin=min(parcel.length.m),
                                              parcel.length.mquantile1=quantile(parcel.length.m,probs = c(0.05)),
                                              parcel.length.mmid=median(parcel.length.m),
                                              parcel.length.maverage=mean(parcel.length.m),
                                              parcel.length.mquantile2=quantile(parcel.length.m,probs = c(0.95)),
                                              parcel.length.mmax=max(parcel.length.m))
+
